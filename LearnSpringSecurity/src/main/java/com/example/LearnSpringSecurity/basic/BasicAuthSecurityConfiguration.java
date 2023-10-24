77e8fe46237bfae993a46ae62b6ada357de3509b
Shashwat <shashwatsinha1999@gmail.com>
2023-10-25 00:16:52 +0530

    Added UserName, Password in the h2 DB

--- LearnSpringSecurity/src/main/java/com/example/LearnSpringSecurity/basic/BasicAuthSecurityConfiguration.java
+++ LearnSpringSecurity/src/main/java/com/example/LearnSpringSecurity/basic/BasicAuthSecurityConfiguration.java
@@ -1,14 +1,19 @@
-/*
-* ...For disabling CSRF Token...
-* */
-
 package com.example.LearnSpringSecurity.basic;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -16,6 +21,9 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 public class BasicAuthSecurityConfiguration {
 
+    /*
+     * ...For disabling CSRF Token...
+     * */
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
@@ -38,5 +46,55 @@ public class BasicAuthSecurityConfiguration {
         return http.build();
     }
 
+    /*
+    * ...Storing UserName and Password in-Memory instead of having it in applications.properties file...
+    * */
+//    @Bean
+//    public UserDetailsService userDetailService() {
+//
+//        var user = User.withUsername("shashwat")
+//                .password("{noop}dummy")
+//                .roles("USER")
+//                .build();
+//
+//
+//        var admin = User.withUsername("admin")
+//                .password("{noop}dummy")
+//                .roles("ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(user, admin);
+//    }
+
+    /*
+    * ...Storing UserName and Password in h2 DB instead of having it in in-Memory...
+    * */
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+                .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailService(DataSource dataSource) {
+
+        var user = User.withUsername("shashwat")
+                .password("{noop}dummy")
+                .roles("USER")
+                .build();
+
+        var admin = User.withUsername("admin")
+                .password("{noop}dummy")
+                .roles("ADMIN", "USER")
+                .build();
+
+        var jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        jdbcUserDetailsManager.createUser(user);
+        jdbcUserDetailsManager.createUser(admin);
+
+        return jdbcUserDetailsManager;
+    }
 
 }
