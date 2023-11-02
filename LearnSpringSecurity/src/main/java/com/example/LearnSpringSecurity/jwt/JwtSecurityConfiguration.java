3dd4d57d07e9f79f172c572d6e9e6ff7f21878f3
Shashwat <shashwatsinha1999@gmail.com>
2023-11-03 00:34:41 +0530

    Added JWT

--- LearnSpringSecurity/src/main/java/com/example/LearnSpringSecurity/jwt/JwtSecurityConfiguration.java
+++ LearnSpringSecurity/src/main/java/com/example/LearnSpringSecurity/jwt/JwtSecurityConfiguration.java
@@ -1,36 +1,50 @@
-package com.example.LearnSpringSecurity.basic;
+package com.example.LearnSpringSecurity.jwt;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPublicKey;
+import java.util.UUID;
+
+import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-import javax.sql.DataSource;
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 
-//@Configuration
-public class BasicAuthSecurityConfiguration {
+@Configuration
+public class JwtSecurityConfiguration {
 
-    /*
-     * ...For disabling CSRF Token...
-     * */
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http.authorizeHttpRequests(
                 auth -> {
-                    auth.anyRequest().authenticated();
+                    auth
+                            .requestMatchers("/authenticate").permitAll()
+                            .anyRequest().authenticated();
                 });
 
         http.sessionManagement(
@@ -39,37 +53,18 @@ public class BasicAuthSecurityConfiguration {
                                 SessionCreationPolicy.STATELESS)
         );
 
-        //http.formLogin();
         http.httpBasic(withDefaults());
 
         http.csrf(csrf -> csrf.disable());
 
+        http.headers(headers -> headers.frameOptions(frameOptionsConfig-> frameOptionsConfig.disable()));
+
+        http.oauth2ResourceServer((oauth2) -> oauth2.jwt(withDefaults()));
+
+
         return http.build();
     }
 
-    /*
-    * ...Storing UserName and Password in-Memory instead of having it in applications.properties file...
-    * */
-//    @Bean
-//    public UserDetailsService userDetailService() {
-//
-//        var user = User.withUsername("shashwat")
-//                .password("{noop}dummy")
-//                .roles("USER")
-//                .build();
-//
-//
-//        var admin = User.withUsername("admin")
-//                .password("{noop}dummy")
-//                .roles("ADMIN")
-//                .build();
-//
-//        return new InMemoryUserDetailsManager(user, admin);
-//    }
-
-    /*
-    * ...Storing UserName and Password in h2 DB instead of having it in in-Memory...
-    * */
     @Bean
     public DataSource dataSource() {
         return new EmbeddedDatabaseBuilder()
@@ -81,7 +76,7 @@ public class BasicAuthSecurityConfiguration {
     @Bean
     public UserDetailsService userDetailService(DataSource dataSource) {
 
-        var user = User.withUsername("shashwat")
+        var user = User.withUsername("in28minutes")
                 //.password("{noop}dummy")
                 .password("dummy")
                 .passwordEncoder(str -> passwordEncoder().encode(str))
@@ -107,4 +102,45 @@ public class BasicAuthSecurityConfiguration {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public KeyPair keyPair() {
+        try {
+            var keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(2048);
+            return keyPairGenerator.generateKeyPair();
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    @Bean
+    public RSAKey rsaKey(KeyPair keyPair) {
+
+        return new RSAKey
+                .Builder((RSAPublicKey)keyPair.getPublic())
+                .privateKey(keyPair.getPrivate())
+                .keyID(UUID.randomUUID().toString())
+                .build();
+    }
+
+    @Bean
+    public JWKSource<SecurityContext> jwkSource(RSAKey rsaKey) {
+        var jwkSet = new JWKSet(rsaKey);
+
+        return (jwkSelector, context) ->  jwkSelector.select(jwkSet);
+
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder(RSAKey rsaKey) throws JOSEException {
+        return NimbusJwtDecoder
+                .withPublicKey(rsaKey.toRSAPublicKey())
+                .build();
+
+    }
+
+    @Bean
+    public JwtEncoder jwtEncoder(JWKSource<SecurityContext> jwkSource) {
+        return new NimbusJwtEncoder(jwkSource);
+    }
 }
